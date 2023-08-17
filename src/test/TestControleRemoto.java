ca2fcb79c90aa6269d7a63569443e1ef294c22b3
Cleber <125226920+clebermarcolino@users.noreply.github.com>
2023-08-17 17:19:56 -0300

    Update TestControleRemoto.java

--- src/test/TestControleRemoto.java
+++ src/test/TestControleRemoto.java
@@ -52,10 +52,6 @@ public class TestControleRemoto {
         String menuOutput = controle.fecharMenu();
         assertEquals("Fechando Menu...", menuOutput);
 
-<<<<<<< HEAD:src/test/TestControleRemoto.java
-
-=======
->>>>>>> 43dac9b44ef18c253711e9f2b24fa535b9d57938:src/main/TestControleRemoto.java
         // Aqui, apenas verificamos se não lança exceção
         assertDoesNotThrow(() -> controle.abrirMenu());
     }
